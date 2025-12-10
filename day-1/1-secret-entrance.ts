0ba578ae3811de66c4816c90cfc893960b3c2d59
Shaquille Kiragu <shaquille@toru.digital>
2025-12-10 17:04:18 +0000

    begin day 1 task 1

---
+++ day-1/1-secret-entrance.ts
@@ -0,0 +1,6 @@
+function secretEntrance(rotations: string[]): number {
+	for (const rotation of rotations) {
+		const direction: string = rotation.charAt(0);
+		const amount: number = Number(rotation.slice(1));
+	}
+}
