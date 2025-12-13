e15662fd92102f6e805accad7e659beb624b8662
Shaquille Kiragu <108951654+shaquillekiragu@users.noreply.github.com>
2025-12-13 20:11:52 +0000

    complete day 1 tasks successfully

--- tasks/day-1-secret-entrance.ts
+++ tasks/day-1-secret-entrance.ts
@@ -24,32 +24,40 @@ function taskOne(rotations: string[]): number {
 	return zero_counter;
 }
 
-console.log(taskOne(rotations_one), " < rotations one zero counter");
-console.log(taskOne(rotations_two), " < rotations two zero counter");
+// console.log(taskOne(rotations_one), " < rotations one zero counter");
+// console.log(taskOne(rotations_two), " < rotations two zero counter");
 
 function taskTwo(rotations: string[]): number {
 	let passing_zero_counter: number = 0;
 	let dial_num: number = 50;
+
+	function incrementZeroCounter(dial_num: number): void {
+		if (dial_num % 100 === 0) {
+			passing_zero_counter++;
+		}
+	}
 
 	for (const rotation of rotations) {
 		const direction: string = rotation.trim().charAt(0);
 		const amount: number = Number(rotation.trim().slice(1));
 
 		if (direction.toUpperCase() === "R") {
-			dial_num += amount;
+			for (let i = 0; i < amount; i++) {
+				dial_num++;
+				incrementZeroCounter(dial_num);
+			}
 		} else if (direction.toUpperCase() === "L") {
-			dial_num -= amount;
+			for (let i = amount; i > 0; i--) {
+				dial_num--;
+				incrementZeroCounter(dial_num);
+			}
 		} else {
 			continue;
-		}
-
-		if (dial_num % 100 === 0) {
-			passing_zero_counter++;
 		}
 	}
 
 	return passing_zero_counter;
 }
 
-console.log(taskTwo(rotations_one), " < rotations one zero counter");
-console.log(taskTwo(rotations_two), " < rotations two zero counter");
+// console.log(taskTwo(rotations_one), " < rotations one zero counter");
+// console.log(taskTwo(rotations_two), " < rotations two zero counter");
