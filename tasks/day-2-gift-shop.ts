e9648d10146e8beca7c35a2bd9b31ebdb976cb08
Shaquille Kiragu <108951654+shaquillekiragu@users.noreply.github.com>
2025-12-19 14:33:16 +0000

    attempt at solving day two task 1

--- tasks/day-2-gift-shop.ts
+++ tasks/day-2-gift-shop.ts
@@ -1,5 +1,5 @@
-const data = require("../data/id-range-list");
-console.log("testing")
+const dataModule = require("../data/id-range-list");
+const data = dataModule.default || dataModule;
 
 function taskOne(id_range_list: string): number {
 	const data_array = id_range_list.split(",");
@@ -13,10 +13,15 @@ function taskOne(id_range_list: string): number {
 
 		for (let i = lower_num; i <= higher_num; i++) {
 			const stringified_num = String(i);
-			const first_string_half = stringified_num.slice(0, stringified_num.length);
-			console.log(first_string_half, " < first half");
-			const second_string_half = stringified_num.slice(stringified_num.length + 1);
-			console.log(second_string_half, " < second half");
+			const first_string_half = stringified_num.slice(
+				0,
+				stringified_num.length / 2
+			);
+			// console.log(first_string_half, " < first half");
+			const second_string_half = stringified_num.slice(
+				stringified_num.length / 2 + 1
+			);
+			// console.log(second_string_half, " < second half");
 
 			if (Number(first_string_half) === Number(second_string_half)) {
 				invalid_id_total += Number(first_string_half);
