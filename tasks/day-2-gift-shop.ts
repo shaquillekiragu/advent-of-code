1c23abf3ced6e23d0a5a3d09fff2c2533449bfa0
Shaquille Kiragu <skiragu77@gmail.com>
2025-12-28 17:38:11 +0000

    wip: day 2 task

--- tasks/day-2-gift-shop.ts
+++ tasks/day-2-gift-shop.ts
@@ -3,6 +3,8 @@ const data = dataModule.default || dataModule;
 
 function taskOne(id_range_list: string): number {
 	const data_array = id_range_list.split(",");
+	console.log(data_array, " < data_array");
+
 	let invalid_id_total: number = 0;
 
 	for (const range of data_array) {
@@ -10,21 +12,27 @@ function taskOne(id_range_list: string): number {
 
 		const lower_num = Number(range.slice(0, hyphen_index));
 		const higher_num = Number(range.slice(hyphen_index + 1));
+		console.log(lower_num, " < lower_num");
+		console.log(higher_num, " < higher_num");
 
 		for (let i = lower_num; i <= higher_num; i++) {
 			const stringified_num = String(i);
+			// console.log(stringified_num, " < stringified num");
+
 			const first_string_half = stringified_num.slice(
 				0,
 				stringified_num.length / 2
 			);
 			// console.log(first_string_half, " < first half");
+
 			const second_string_half = stringified_num.slice(
-				stringified_num.length / 2 + 1
+				stringified_num.length / 2
 			);
 			// console.log(second_string_half, " < second half");
 
 			if (Number(first_string_half) === Number(second_string_half)) {
-				invalid_id_total += Number(first_string_half);
+				// console.log("EQUALS");
+				invalid_id_total += Number(stringified_num);
 			}
 		}
 	}
